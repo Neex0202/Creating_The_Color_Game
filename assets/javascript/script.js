1ffdebeac924fafdfc8f6c9078ff8c47e3765f1b
nico sta. ana <nstaana@hotmail.com>
2018-06-10 13:08:27 -0400

    added rng for color picker, added changeColor()

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,8 +10,9 @@ var colors = [
 ]
 // Declare Variables
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[5];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -32,11 +33,35 @@ for(var i = 0; i < squares.length; i++){
 		// compare color to pickedColor
 			if(clickedColor === pickedColor){
 				console.log("Correct!");
+				messageDisplay.textContent = "Correct";
+
+				// change squares to correct color;
+				changeColors(clickedColor);
+
 			}else{
 				console.log("WRONG!!!")
+				// change background of wrong square to match .container background
 				this.style.background = "#232323";
+				messageDisplay.textContent = "Try Again"
 			}
 	});
 
 }
 
+
+// function to change all square colors to correct color. Called when correct square chosen
+function changeColors(color){
+	// loop through all squares
+	for(var i = 0; i <squares.length; i++){
+	// change each color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+
+
+// RNG function to pick color
+function pickColor() {
+	var rng = Math.floor(Math.random() * colors.length);
+	return colors[rng];
+}
